0c8e501e5e69fa2b520723cbd0b29f0e991419af
Danilo Cardoso <danilocalessa@gmail.com>
2017-08-29 16:12:13 -0300

    pacotes de validações

---
+++ Validacoes/src/cpf/Teste.java
@@ -0,0 +1,20 @@
+package cpf;
+
+import java.util.Scanner;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		String cpf;
+		System.out.println("Digite se cpf");
+		cpf = in.next();
+
+		if (ValidaCPF.isCPF(cpf) == true) {
+			System.out.println(ValidaCPF.imprimeCPF(cpf));
+		} else {
+			System.out.println("CPF inválido	");
+		}
+	}
+}
